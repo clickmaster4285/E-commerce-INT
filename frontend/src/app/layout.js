4170d13d516efd746a1d6c9e4f48dbcc3b504f86
clickmasters <ahsan9090746>
2026-07-31 11:17:29 +0500

    fix the error

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -5,7 +5,7 @@ import Providers from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Inventory App"
+  title: "Inventory App",
   description: "Inventory Management System",
 };
 
