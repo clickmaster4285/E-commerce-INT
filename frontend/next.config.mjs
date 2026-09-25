d6d43d9751c898e90687533d7ec2a618d87bae84
clickmasters <ahsan9090746>
2026-09-25 15:10:58 +0500

    merging

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
-    '192.168.88.64',
+    '192.168.88.73',
     'localhost',
     '127.0.0.1',
   ],
